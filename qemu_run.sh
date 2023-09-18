4af0c7088234ef926e93e00836f9ca8a601d491d
beatdjam <beat3526@gmail.com>
2023-09-18 15:00:13 +0900

    Day1 Hello World on QEMU

--- qemu_run.sh
+++ qemu_run.sh
@@ -1,4 +1,5 @@
 cargo build --release
+rm -rf ~/path/to/EFI
+mkdir -p ~/path/to/EFI/BOOT
 cp target/x86_64-unknown-uefi/release/rusty-mikan-os.efi ~/path/to/EFI/BOOT/bootx64.efi
-cd $HOME
-qemu-system-x86_64 -bios path/to/OVMF.fd -drive format=raw,file=fat:rw:path/to/
+qemu-system-x86_64 -bios $HOME/path/to/OVMF.fd -drive format=raw,file=fat:rw:$HOME/path/to/

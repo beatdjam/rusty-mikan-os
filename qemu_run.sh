d175d8d600a0255e8f6224483c5f73a6951a10ca
beatdjam <beat3526@gmail.com>
2023-09-19 21:10:42 +0900

    Day1 Hello World on QEMU

--- qemu_run.sh
+++ qemu_run.sh
@@ -1,5 +1,8 @@
 cargo build --release
-rm -rf ~/path/to/EFI
-mkdir -p ~/path/to/EFI/BOOT
-cp target/x86_64-unknown-uefi/release/rusty-mikan-os.efi ~/path/to/EFI/BOOT/bootx64.efi
-qemu-system-x86_64 -bios $HOME/path/to/OVMF.fd -drive format=raw,file=fat:rw:$HOME/path/to/
+qemu-img create -f raw disk.img 200M
+mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
+hdiutil attach disk.img
+mkdir -p /Volumes/MIKAN\ OS/EFI/BOOT
+cp target/x86_64-unknown-uefi/release/rusty-mikan-os.efi /Volumes/MIKAN\ OS/EFI/BOOT/BOOTX64.EFI
+hdiutil detach /Volumes/MIKAN\ OS
+qemu-system-x86_64 -drive if=pflash,format=raw,file=$HOME/ghq/github.com/uchan-nos/mikanos-build/devenv/OVMF_CODE.fd -drive if=pflash,format=raw,file=$HOME/ghq/github.com/uchan-nos/mikanos-build/devenv/OVMF_VARS.fd -hda disk.img
